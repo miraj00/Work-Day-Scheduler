43275c6b746268fddff6dd3dcac73ea4dd121b87
miraj <miraj00@gmail.com>
2021-07-16 20:00:12 -0400

    working on local-storage

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,22 +1,58 @@
 setInterval(function() {
     var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
     //console.log(currentTime);
-    var x = document.getElementById("currentDay").innerHTML = currentTime;
-    //console.log(x); 
-     }, 1000);
+    document.getElementById("currentTimeStyle").innerHTML = currentTime;
+  }, 1000);
 
 
-//---------------------------------------------------------------------------------------
 
+     
 //  text written in text area need to store in local storage upon pressing store icon
 
-var tasks = {};
+
+var task0 = {};
+
+$ (".saveBtn1").click(function() {
+    // get form values
+     var taskText = $(#task0).val();
+    
+    tasks.push({ 
+         text: tastText, });
+    localStorage.setItem ("tasks", JSON.stringify(taskText));
+});
 
 
+
+
+
+
+
+
+
+
+/*
+$ (".saveBtn1").click(function() {
+    // get form values
+    console.log(task0);
+    var taskText = $("#task0").val();
+    ;
+    console.log(taskText);
+})
+
+//var tasks = {};
+//console.log(tasks);
 
 var saveTasks = function() {
-localStorage.setItem ("tasks", JSON.stringify(tasks));
+localStorage.setItem ("tasks", JSON.stringify(taskText));
 };
+
+
+
+
+
+*/
+
+
 
 
 // the text are changes color as time goes from 9 am to 5pm 
